4f32cb45b8e3879c5fa41ad5b3889d956e96ce09
briltz <jake.briltz12@gmail.com>
2020-09-09 18:04:43 -0400

    Updates

---
+++ precache-manifest.218b7fcd628672bd2ff94fd3cd43f746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e7753cf9bc268fc8da9c896f5f4098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9858c1534c58f06feb",
+    "url": "/static/css/main.e30f4b28.chunk.css"
+  },
+  {
+    "revision": "1504712449ecf2a1a3ad",
+    "url": "/static/js/2.e68087fa.chunk.js"
+  },
+  {
+    "revision": "ccb96163cb81453816364e1c2beb6525",
+    "url": "/static/js/2.e68087fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9858c1534c58f06feb",
+    "url": "/static/js/main.3c8864f1.chunk.js"
+  },
+  {
+    "revision": "aa15ee69cc8c33a0889a",
+    "url": "/static/js/runtime-main.e0e498bf.js"
+  }
+]);
