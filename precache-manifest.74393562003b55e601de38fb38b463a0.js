9457c56b6a6cf29b9fdfd7c43b0d29f3222cd8ee
briltz <jake.briltz12@gmail.com>
2020-09-09 16:12:57 -0400

    Updates

---
+++ precache-manifest.74393562003b55e601de38fb38b463a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b1cc26d360bc8b6e6812103ef68d39",
+    "url": "/wyvern-stocks/index.html"
+  },
+  {
+    "revision": "6f07122254fa421eded7",
+    "url": "/wyvern-stocks/static/css/main.e30f4b28.chunk.css"
+  },
+  {
+    "revision": "1505815135651a66224c",
+    "url": "/wyvern-stocks/static/js/2.8475a059.chunk.js"
+  },
+  {
+    "revision": "ccb96163cb81453816364e1c2beb6525",
+    "url": "/wyvern-stocks/static/js/2.8475a059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f07122254fa421eded7",
+    "url": "/wyvern-stocks/static/js/main.b503eb58.chunk.js"
+  },
+  {
+    "revision": "d483003c30e34563f909",
+    "url": "/wyvern-stocks/static/js/runtime-main.b67a8708.js"
+  }
+]);
